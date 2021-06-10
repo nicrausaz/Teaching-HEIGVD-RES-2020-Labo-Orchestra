602285905e064d6d09cd77a17cdc86505da4757a
Nicolas Crausaz <n.crausaz99@gmail.com>
2021-06-10 11:02:29 +0200

    Install in docker files & refactor

--- docker/image-auditor/src/app.js
+++ docker/image-auditor/src/app.js
@@ -1,46 +1,51 @@
 const dgram = require('dgram')
+const net = require('net')
 const client = dgram.createSocket('udp4')
 
-// Map for musicians
+// Map for musicians play status
 let musicians = new Map()
 
 // Registered sounds & their instruments
-let soundFromInstrument = new Map
-soundFromInstrument.set('ti-ta-ti', { instrument: 'piano' })
-soundFromInstrument.set('pouet', { instrument: 'trumpet' })
-soundFromInstrument.set('trulu', { instrument: 'flute' })
-soundFromInstrument.set('gzi-gzi', { instrument: 'violin' })
-soundFromInstrument.set('boum-boum', { instrument: 'drum' })
+let soundFromInstrument = new Map([
+  ['ti-ta-ti', 'piano'],
+  ['pouet', 'trumpet'],
+  ['trulu', 'flute'],
+  ['gzi-gzi', 'violin'],
+  ['boum-boum', 'drum']
+])
 
 client.on('error', (err) => {
-  console.log(`client error:\n${err.stack}`);
-  client.close();
-});
+  if (err.code === 'ECONNRESET') {
+    console.log('client disconnected')
+  }
+  else {
+    console.log(`client error:\n${err.stack}`)
+    client.close()
+  }
+})
 
 client.on('message', (msg, rinfo) => {
-  console.log(`client got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+  console.log(`client got: ${msg} from ${rinfo.address}:${rinfo.port}`)
   const messageParsed = JSON.parse(msg.toString())
-  musicians.set(messageParsed.uuid, { instrument: soundFromInstrument.get(messageParsed.sound).instrument, activeSince: new Date() })
-});
+  musicians.set(messageParsed.uuid, { instrument: soundFromInstrument.get(messageParsed.sound), activeSince: new Date() })
+})
 
 client.on('listening', () => {
-  const address = client.address();
-  console.log(`client listening ${address.address}:${address.port}`);
-});
+  console.log(`client listening ${client.address().address}:${client.address().port}`)
+})
 
-client.bind(11111);
+client.bind(11111)
 
 // Partie TCP pour obtenir la map en JSON
-var net = require('net');
 
-var server = net.createServer(function (socket) {
+const server = net.createServer(socket => {
   // Construction du tableau de retour
-  const array = new Array()
-  musicians.forEach((value, key) => array.push({uuid: key, instrument: value.instrument, activeSince: value.activeSince}))
+  const data = []
+  musicians.forEach((value, key) => data.push({ uuid: key, instrument: value.instrument, activeSince: value.activeSince }))
 
-  socket.write(Buffer.from(JSON.stringify(array)));
-  console.log(`sending: ${array}`);
-  socket.pipe(socket);
-});
+  socket.write(Buffer.from(JSON.stringify(data)))
+  console.log(`sending: ${data}`)
+  socket.pipe(socket)
+})
 
-server.listen(2205, 'localhost');
+server.listen(2205, 'localhost')
